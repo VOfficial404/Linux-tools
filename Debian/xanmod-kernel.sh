99ba58c37866cc9ddc539d8301dbf86ccf06e2c2
V <104217168+VOfficial404@users.noreply.github.com>
2024-12-17 19:06:00 +0800

    Update xanmod-kernel.sh

--- Debian/xanmod-kernel.sh
+++ Debian/xanmod-kernel.sh
@@ -6,7 +6,7 @@ wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -vo /usr/share/keyr
 
 echo 'deb [signed-by=/usr/share/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
 
-apt-get update && apt-get install linux-xanmod-x64v3
+apt-get update && apt-get install linux-xanmod-x64v3 -y
 
 echo "net.ipv4.tcp_congestion_control = bbr" >> /etc/sysctl.conf
 echo "net.core.default_qdisc = fq_pie" >> /etc/sysctl.conf
