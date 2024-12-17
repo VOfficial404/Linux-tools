796e547323f158143e9db1472f979efe4ab510ca
V <104217168+VOfficial404@users.noreply.github.com>
2024-12-17 19:39:01 +0800

    Update xanmod-kernel.sh

--- Debian/xanmod-kernel.sh
+++ Debian/xanmod-kernel.sh
@@ -7,8 +7,8 @@ swapon /swapfile
 swapon --show
 cp /etc/fstab /etc/fstab.bak
 echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-echo "已自动创建1G的SWAP 防止小内存机器失联"
 echo "vm.swappiness = 100" >> /etc/sysctl.conf
+echo "已自动创建1G的SWAP 防止小内存机器失联"
 
 apt-get update -y && apt-get dist-upgrade curl wget unzip gpg -y
 
